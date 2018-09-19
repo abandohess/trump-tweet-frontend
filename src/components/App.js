70db3910b49837eea5b8643533ad95daf6863155
Chris Bertasi <christopher.p.bertasi.19@dartmouth.edu>
2018-09-19 19:37:48 -0400

    addition of delete button

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     };
     this.onPhoneNumberChange = this.onPhoneNumberChange.bind(this);
     this.onSubmitPhoneNumber = this.onSubmitPhoneNumber.bind(this);
+    this.onDeletePhoneNumber = this.onDeletePhoneNumber.bind(this);
     // this.showModal = this.showModal.bind(this);
   }
 
@@ -46,6 +47,21 @@ class App extends Component {
     });
   }
 
+  onDeletePhoneNumber() {
+    if (!isValidPhoneNumber(this.state.phoneNumber)) return;
+    axios.delete(`${ROOT_URL}/users`, { phoneNumber: this.state.phoneNumber.replace(/\D/g, '') }).then((response) => {
+      console.log(response);
+      if (response.data === 'Successfully deleted phone number\n') {
+        alert('Successfully deleted phone number');
+      } else {
+        alert('Failed to delete phone number, ', this.state.phoneNumber);
+      }
+    }).catch((error) => {
+      console.log(error);
+      alert('Failed to delete phone number :( please try again)');
+    });
+  }
+
   // showModal() {
   //   document.getElementById('privacy-policy').click();
   // }
@@ -64,9 +80,14 @@ class App extends Component {
           <p className="enter-text-label">Enter your phone number</p>
           <input className="input-bar" placeholder="xxx-xxx-xxxx" onChange={this.onPhoneNumberChange} value={this.state.phoneNumber} />
 
-          <Button variant="extendedFab" aria-label="Delete" className="sign-up-button" onClick={this.onSubmitPhoneNumber}>
-          Sign Up
-          </Button>
+          <div className="button-container">
+            <Button variant="extendedFab" aria-label="Delete" className="sign-up-button" onClick={this.onSubmitPhoneNumber}>
+              <span style={{ display: 'inline-block', width: 11 }} />Subscribe<span style={{ display: 'inline-block', width: 11 }} />
+            </Button>
+            <Button variant="extendedFab" aria-label="Delete" className="unsubscribe-button" onClick={this.onDeletePhoneNumber}>
+              Unsubscribe
+            </Button>
+          </div>
         </div>
         <hr className="divider" />
 
